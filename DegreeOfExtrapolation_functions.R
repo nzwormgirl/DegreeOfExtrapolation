ceed0ba835b545e3017d0e8bc92112265ebf4d4f
nzwormgirl <amylouisewhitehead@gmail.com>
2018-05-22 10:35:03 +1200

    Updates to code and functions to add calculations of Degree of Extrapolation for MARS models

--- DegreeOfExtrapolation_functions.R
+++ DegreeOfExtrapolation_functions.R
@@ -2,6 +2,7 @@
 #   Model fitting and testing; interpolation-extrapolation   #
 ##############################################################
 
+# IMPORTANT; Must be using 32bit R. For gradient forests and extended forests to get x transformed to turnover in y space
 
 ##############################################################
 #                       Functions                            #
@@ -26,10 +27,6 @@
 #########
 RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   # 
   
-  # Packages
-  require(randomForestSRC)                # for random forest fitting including many detailed outputs
-  require(gradientForest)                 # Must be using 32bit R. For gradient forests and extended forests to get x transformed to turnover in y space
-  
   if(any(sapply(myFrame[, c(myY, myX)],    function(x) is.na(x)))) stop("NA's found in myFrame")
   if(any(sapply(myFrameOutside[, c( myX)], function(x) is.na(x)))) stop("NA's found in myFrameOutside")
   
@@ -38,7 +35,7 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
   OutsideRowNames <- rownames(myFrameOutside)
   rownames(myFrame) <- as.character(1:nrow(myFrame))
   rownames(myFrameOutside) <- as.character(1:nrow(myFrameOutside))
-
+  
   #########
   # function to rbind all the elements in a list of matrices
   # dealing with nonunique rownames
@@ -50,14 +47,14 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
     return(myOut)
   }  
   #########
-
+  
   print(paste("randomForest for", myY))
   
   RFmodel <- rfsrc(formula(paste(myY, paste(myX, collapse = "+"), sep = "~")), data = myFrame, membership = T, ...) #  ntree = 100, importance = "permute"
   
   rownames(RFmodel$membership) <- rownames(myFrame)# terminal node for each obs
   rownames(RFmodel$inbag) <-      rownames(myFrame)# number of times each obs is in each tree
-
+  
   ######### 
   # ObsNumber = vector of rownames of observations
   # n = tree number
@@ -162,7 +159,7 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
   #########
   
   print(paste("WeightListOutside for", myY))
-  WeightListOutside <- lapply(1:RFmodel$ntree, GetW4 ) # Doug.rbind.list()         
+  WeightListOutside <- lapply(1:RFmodel$ntree, GetW4 )         
   print(paste("rbinding WeightListOutside for", myY))
   Doug <- lapply(WeightListOutside, function(dfs) do.call("rbind", lapply(dfs, function(df) df)))
   Doug <- do.call("rbind", lapply(Doug, function(df) df))
@@ -170,7 +167,7 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
   # weightings on the fitted observations (rows) that are to be used for the new predictions (columns), standardised by number of trees
   print(paste("tapplying WeightListOutside for", myY))
   WeightingMatOutside <- tapply(Doug[,"WeightInbag"], list( factor(ObsName, levels = rownames(myFrame)), Doug[,"PredNameTo"]), sum, na.rm = T)/ RFmodel$ntree
-
+  
   WeightingMatOutside <- WeightingMatOutside[, rownames(myFrameOutside)]
   rm(Doug, ObsName, WeightList, WeightListOutside); gc()
   
@@ -181,7 +178,7 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
   print(paste("gradientForest for", myY))
   GFmodel <- gradientForest(myFrame, predictor.vars = myX, response.vars = myY, mtry = RFmodel$mtry, ntree = RFmodel$ntree )  
   GFpredInside <-  predict(GFmodel, extrap = T) #  It is a dataframe in which each predictor has been transformed to the biological scale by the cumulative importance function, as defined by cumimp.
-
+  
   # distances in X with respect to y for fitted data. Could use various methods. using default method = "euclidean"
   myFrame$WeightedDistBlind    <-      apply(t(WeightingMatInsideOOB)   * as.matrix(dist(GFpredInside)), 2, sum, na.rm = T) # distances weighted by oob trees only
   
@@ -208,11 +205,11 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
   myFrameOutside$Source <- "New data"
   
   myOutVars <- c("PredBlind","Source", "WeightedDistBlind", "WeightedDist2step") 
-
+  
   # re-install rownames
   rownames(myFrame) <- InsideRowNames 
   rownames(myFrameOutside) <-OutsideRowNames
-
+  
   myOutFrame <- rbind(myFrame[, myOutVars], myFrameOutside[, myOutVars])
   
   # degree of extrapolation
@@ -226,56 +223,214 @@ RFInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {   #
 #########
 
 
-##############################################################
-#                       Input data                           #
-##############################################################
-
-load("FittedDataList.RData")
-
 
 ##############################################################
-#                    Run example, NZ                         #
+#       MARS-earth degree of extrapolation functions         #
 ##############################################################
 
-head(FittedDataList[[1]])
+#########
+GetCV <- function(myRow, myY, myX, myFrame, ...) { # myModel, 
+  print(myRow)
+  myModel <- earth(formula(paste(myY, paste(myX, collapse = "+"), sep = "~")), data = myFrame[-myRow, ], keepxy = T,  ...)  
+  myNewData <- myFrame[myRow, ] 
+  if(!any(grepl("*", rownames(myModel$coefficients), fixed = T))){
+    
+    xVar <- c(myModel$namesx,"(Intercept)")[apply(sapply(c(myModel$namesx,"(Intercept)"), function(x) grepl(x, rownames(myModel$coefficients))) , 1, which)]
+    TermFrame <- data.frame(Terms = rownames(myModel$coefficients), namesx = xVar)
+    gcvImp <- evimp(myModel)[, "gcv"]/100 # importances
+    
+    #########
+    GetxVal <- function(myChar) {
+      myDesc <- myChar[1]
+      myxVar <- myChar[2]
+      if(myDesc == "(Intercept)") {
+        myX <- NA
+      } else {
+        TermFrame <- strsplit(myDesc, split = "-")[[1]]
+        myLR <- c("right","left")[grep(myxVar, TermFrame)]
+        if(myLR == "right") {
+          myX <- substr(TermFrame[2], 1, nchar(TermFrame[2])-1)
+        }
+        if(myLR == "left") {
+          myX <- substr(TermFrame[1], 3, nchar(TermFrame[2]))
+        }
+        myX <- as.numeric(myX)
+      }
+      return(myX)
+    }
+    #########
+    TermFrame$Xval <- apply(TermFrame, 1, GetxVal)
+    DistFrameNEW <- data.frame(t(sapply(myModel$namesx, function(x) rep(NA, nrow(myNewData))) ))
+    rownames(DistFrameNEW) <- rownames(myNewData)
+    DistFrameTRAIN <- data.frame(sapply(myModel$namesx, function(x) rep(NA, nrow(myModel$data))) )
+    rownames(DistFrameTRAIN) <- rownames(myModel$data)
+    myListZero <- vector(mode = "list")
+    for(myTerm in myModel$namesx) myListZero[[myTerm]] <- 0
+    xPointList <- vector(mode = "list")
+    for(myTerm in myModel$namesx) {
+      x <- TermFrame$Xval[TermFrame$namesx == myTerm]
+      x <- c(x, range(myModel$data[[myTerm]], na.rm = T))
+      x <- unique(x)
+      x <- sort(x)
+      myListOne <- myListZero
+      myListOne[[myTerm]] <- x
+      y <- predict(myModel,  expand.grid(myListOne))
+      xPointList[[myTerm]] <- data.frame(x,y)
+      names(xPointList[[myTerm]]) <- c("x","y")
+      xPointList[[myTerm]]$yDiff <- c(0, diff(xPointList[[myTerm]]$y))                                     # differences in y with x
+      xPointList[[myTerm]]$yCumDiff <- cumsum(abs(xPointList[[myTerm]]$yDiff))                             # cumulative differences in y 
+      xPointList[[myTerm]]$yCumDiff <- xPointList[[myTerm]]$yCumDiff / max(xPointList[[myTerm]]$yCumDiff)  # range standardise
+      xPointList[[myTerm]]$yCumDiff <- xPointList[[myTerm]]$yCumDiff * gcvImp[myTerm]                      # scale by importance
+      
+      # print(xPointList[[myTerm]])
+      if(all(is.na(xPointList[[myTerm]]$yCumDiff))) {
+        DistFrameNEW[[myTerm]]   <- 0
+        DistFrameTRAIN[[myTerm]] <- rep(0, nrow(myModel$data))
+      } else {
+        DistFrameNEW[[myTerm]]   <- approx(x = xPointList[[myTerm]]$x, y = xPointList[[myTerm]]$yCumDiff, xout = myNewData[[myTerm]])$y
+        DistFrameTRAIN[[myTerm]] <- approx(x = xPointList[[myTerm]]$x, y = xPointList[[myTerm]]$yCumDiff, xout = myModel$data[[myTerm]])$y
+      }
+      # outside training range
+      if(any(is.na(DistFrameNEW[[myTerm]]))) {
+        MeanSlope <- (max(xPointList[[myTerm]]$yCumDiff) - min(xPointList[[myTerm]]$yCumDiff)) / (max(xPointList[[myTerm]]$x) - min(xPointList[[myTerm]]$x))
+        myNaRows <- which(is.na(DistFrameNEW[[myTerm]]))
+        DistFrameNEW[[myTerm]][myNaRows] <- MeanSlope*( myNewData[[myTerm]][myNaRows] - min(xPointList[[myTerm]]$x) )  # myNewData[[myTerm]][myNaRows] 
+      }
+    }
+    
+    # distances between all pairs of training data
+    DistTRAIN <- apply(as.matrix(dist(DistFrameTRAIN)), 2, sum, na.rm = T)
+    # distances between new and training data
+    DistNEW <- as.vector(sapply(1:nrow(DistFrameNEW) , function(n) sum(as.matrix(dist(rbind(DistFrameNEW[n, ], DistFrameTRAIN)))[1,-1] ))) 
+    DistMEAN <- mean(DistTRAIN)
+    DistTRAIN <- (DistTRAIN - DistMEAN) / DistMEAN
+    DistNEW   <- (DistNEW - DistMEAN) / DistMEAN
+  } else {
+    DistNEW <- NA
+  }
+  
+  PredBlind <- predict(myModel, newdata = myFrame[myRow, ])
+  
+  myOut <- data.frame(PredBlind = PredBlind, DegreeExtrap = DistNEW)
+  return(myOut)
+}
+#########
 
-myVar <- "FRE3"
-DoEframe <- RFInterpExterp(myY = names(FittedDataList[[myVar]])[1], 
-                           myX = names(FittedDataList[[myVar]])[3:10], 
-                           myFrame        = FittedDataList[[myVar]] [FittedDataList[[myVar]]$Island == "South", ], 
-                           myFrameOutside = FittedDataList[[myVar]] [FittedDataList[[myVar]]$Island == "North", ], 
-                           ntree = 500, importance = "permute")
+#' This function allows you to fit a MARS regression model and assess degree of extrapolation when predicting to a new set of data 
+#' @param myY Name of response variable (must be a column name of numeric columns of myFrame).
+#' @param myX Vector of predictor variable names (must be column names of numeric columns of myFrame).
+#' @param myFrame A dataframe for the MARS model to fit to.
+#' @param myFrameOutside A dataframe for the MARS to predict to.
+#' @param ... Further arguments to be used in earth function
+#' @keywords random forest
+#' @export
+#' @examples
+#' EarthInterpExterp()
+#' @return  a list containing the following two elements. The first element (called MARSmodel) is the output object from earth. 
+#' The second element (called WeightFrame) is a dataframe containing three columns. PredBlind contains the predicted values.
+#' Source identifies between leave-one-out (LOO) predictions and new predictions.
+#' DegreeExtrap contains the Degree of Extrapolation describing tendency towards extrapolation (positive values) versus interpolation (negative values). 
+#' @details EarthInterpExterp requires the response and all predictor to be continuous numeric variables, each containing no NA values. It currently only works for earth models with no interactions (degree = 1).
 
-# add some variables for plotting
-MapVars <- c("Island","segYmidpoint","segXmidpoint")
+#########
+EarthInterpExterp <- function(myY, myX, myFrame, myFrameOutside, ...) {
+  myModel <- earth(formula(paste(myY, paste(myX, collapse = "+"), sep = "~")), data = myFrame, keepxy = T,  ...)  
+  # add predictions
+  myFrame$PredFitted <- as.vector(myModel$fitted.values)       # fitted predictions
+  
+  x <- lapply(1:nrow(myFrame), GetCV, myY = myY, myX = myX, myFrame = myFrame, ...)   # leave-one-out predictions & degree of extrap  # 
+  x <- data.frame(do.call("rbind", lapply(x, function(df) df)))
+  myFrame[, c("PredBlind","DegreeExtrap")] <- x
+  
+  myFrameOutside$PredBlind <- predict(myModel, newdata = myFrameOutside) #
+  myFrameOutside$DegreeExtrap <- GetDegreeExtrapEarth(myModel, myNewData = myFrameOutside)
+  print("done fitted preds")
+  
+  myFrame$Source        <- "LOO" 
+  myFrameOutside$Source <- "New data"
+  myOutVars <- c("PredBlind","Source","DegreeExtrap") # 
+  myOutFrame <- rbind(myFrame[, myOutVars], myFrameOutside[, myOutVars])
+  return(list(MARSmodel = myModel, WeightFrame = myOutFrame))  
+}
+#########
 
-DoEframe$WeightFrame[, MapVars] <- FittedDataList[[myVar]][ match(rownames(DoEframe$WeightFrame) , rownames(FittedDataList[[myVar]])) , MapVars]
 
-# histogram showing distribution of degree of extrapolation
-histogram(~DegreeExtrap|Source, data = DoEframe$WeightFrame, col = "white", layout = c(1,2))
+#########
+GetDegreeExtrapEarth <- function(myModel, myNewData) {
+  
+  if(!any(grepl("*", rownames(myModel$coefficients), fixed = T))){
+    xVar <- c(myModel$namesx,"(Intercept)")[apply(sapply(c(myModel$namesx,"(Intercept)"), function(x) grepl(x, rownames(myModel$coefficients))) , 1, which)]
+    TermFrame <- data.frame(Terms = rownames(myModel$coefficients), namesx = xVar)
+    gcvImp <- evimp(myModel)[, "gcv"]/100 # importances
+    
+    #########
+    GetxVal <- function(myChar) {
+      myDesc <- myChar[1]
+      myxVar <- myChar[2]
+      if(myDesc == "(Intercept)") {
+        myX <- NA
+      } else {
+        TermFrame <- strsplit(myDesc, split = "-")[[1]]
+        myLR <- c("right","left")[grep(myxVar, TermFrame)]
+        if(myLR == "right") {
+          myX <- substr(TermFrame[2], 1, nchar(TermFrame[2])-1)
+        }
+        if(myLR == "left") {
+          myX <- substr(TermFrame[1], 3, nchar(TermFrame[2]))
+        }
+        myX <- as.numeric(myX)
+      }
+      return(myX)
+    }
+    #########
+    TermFrame$Xval <- apply(TermFrame, 1, GetxVal)
+    DistFrameNEW <- data.frame(sapply(myModel$namesx, function(x) rep(NA, nrow(myNewData))) )
+    rownames(DistFrameNEW) <- rownames(myNewData)
+    DistFrameTRAIN <- data.frame(sapply(myModel$namesx, function(x) rep(NA, nrow(myModel$data))) )
+    rownames(DistFrameTRAIN) <- rownames(myModel$data)
+    myListZero <- vector(mode = "list")
+    for(myTerm in myModel$namesx) myListZero[[myTerm]] <- 0
+    xPointList <- vector(mode = "list")
+    for(myTerm in myModel$namesx) {
+      x <- TermFrame$Xval[TermFrame$namesx == myTerm]
+      x <- c(x, range(myModel$data[[myTerm]], na.rm = T))
+      x <- unique(x)
+      x <- sort(x)
+      myListOne <- myListZero
+      myListOne[[myTerm]] <- x
+      y <- predict(myModel,  expand.grid(myListOne))
+      xPointList[[myTerm]] <- data.frame(x,y)
+      names(xPointList[[myTerm]]) <- c("x","y")
+      xPointList[[myTerm]]$yDiff <- c(0, diff(xPointList[[myTerm]]$y))                                     # differences in y with x
+      xPointList[[myTerm]]$yCumDiff <- cumsum(abs(xPointList[[myTerm]]$yDiff))                             # cumulative differences in y 
+      xPointList[[myTerm]]$yCumDiff <- xPointList[[myTerm]]$yCumDiff / max(xPointList[[myTerm]]$yCumDiff)  # range standardise
+      xPointList[[myTerm]]$yCumDiff <- xPointList[[myTerm]]$yCumDiff * gcvImp[myTerm]                      # scale by importance
+      if(all(is.na(xPointList[[myTerm]]$yCumDiff))) {
+        DistFrameNEW[[myTerm]]   <- rep(0, nrow(myNewData))
+        DistFrameTRAIN[[myTerm]] <- rep(0, nrow(myModel$data))
+      } else {
+        DistFrameNEW[[myTerm]]   <- approx(x = xPointList[[myTerm]]$x, y = xPointList[[myTerm]]$yCumDiff, xout = myNewData[[myTerm]])$y
+        DistFrameTRAIN[[myTerm]] <- approx(x = xPointList[[myTerm]]$x, y = xPointList[[myTerm]]$yCumDiff, xout = myModel$data[[myTerm]])$y
+      }
+      # outside training range
+      if(any(is.na(DistFrameNEW[[myTerm]]))) {
+        MeanSlope <- (max(xPointList[[myTerm]]$yCumDiff) - min(xPointList[[myTerm]]$yCumDiff)) / (max(xPointList[[myTerm]]$x) - min(xPointList[[myTerm]]$x))
+        myNaRows <- which(is.na(DistFrameNEW[[myTerm]]))
+        DistFrameNEW[[myTerm]][myNaRows] <- MeanSlope*( myNewData[[myTerm]][myNaRows] - min(xPointList[[myTerm]]$x) )  # myNewData[[myTerm]][myNaRows] 
+      }
+    }
+    # distances between all pairs of training data
+    DistTRAIN <- apply(as.matrix(dist(DistFrameTRAIN)), 2, sum, na.rm = T)
+    # distances between new and training data
+    DistNEW <- as.vector(sapply(1:nrow(DistFrameNEW) , function(n) sum(as.matrix(dist(rbind(DistFrameNEW[n, ], DistFrameTRAIN)))[1,-1] ))) 
+    DistMEAN <- mean(DistTRAIN)
+    DistTRAIN <- (DistTRAIN - DistMEAN) / DistMEAN
+    DistNEW   <- (DistNEW - DistMEAN) / DistMEAN
+  } else {
+    DistNEW <- NA
+  }
+  return(DistNEW)
+}
+#########
 
-# a map of degree of extrapolation.
-library(lattice)
-trellis.par.set(name = "superpose.symbol", value = list(col = rainbow(12), pch = 16))
-xyplot(segYmidpoint~ segXmidpoint|Source, data = DoEframe$WeightFrame, groups = cut(DegreeExtrap, breaks=10), auto.key = list(space = "right"), scales = list(draw = F))
-
-
-##############################################################
-#                  Run example, generic                      #
-##############################################################
-
-library(dplyr)
-
-# RFInterpExterp requires that all NA's are removed from its input dataframes.
-airquality <- airquality %>% na.omit
-
-# fit to all data except month 5, predicted to month 5. 
-DoEframe <- RFInterpExterp(myY = "Ozone", 
-                           myX = c("Solar.R", "Wind", "Temp"), 
-                           myFrame        = airquality[airquality$Month != 5, ], 
-                           myFrameOutside = airquality[airquality$Month == 5, ], 
-                           ntree = 500, importance = "permute")
-
-# histogram showing distribution of degree of extrapolation
-histogram(~DegreeExtrap|Source, data = DoEframe$WeightFrame, col = "white", layout = c(1,2))
 
---
+++ RunDegreeExtrapolation_RF&MARS.R
@@ -0,0 +1,105 @@
+##############################################################
+#   Model fitting and testing; interpolation-extrapolation   #
+##############################################################
+
+# IMPORTANT; Must be run using 32bit R. 
+# install.packages("gradientForest", repos="http://R-Forge.R-project.org")
+
+
+# Packages
+library(randomForestSRC)                # for random forest fitting including many detailed outputs
+library(gradientForest)                 # Must be using 32bit R.
+library(earth)                          # for MARS models
+library(dplyr)                          # for air quality dataset
+library(lattice)                        # for trellis plots
+
+
+source("DegreeofExtrapolation_functions.R")
+
+##############################################################
+#                       Input data                           #
+##############################################################
+
+# NZ datasets
+load("FittedDataList.RData")
+
+# a generic test dataset
+# RFInterpExterp requires that all NA's are removed from its input dataframes.
+airquality <- airquality %>% na.omit
+
+##############################################################
+# Run example, generic, random forest degree of extrapolation#
+##############################################################
+
+# fit to all data except month 5, predicted to month 5. 
+DoEframeRF_airquality <- RFInterpExterp(myY = "Ozone", # response variable
+                           myX = c("Solar.R", "Wind", "Temp"), # predictor variables
+                           myFrame        = airquality[airquality$Month != 5, ], # training data
+                           myFrameOutside = airquality[airquality$Month == 5, ], # new data
+                           ntree = 500, importance = "permute") # random forest settings
+
+# histogram showing distribution of degree of extrapolation
+histogram(~DegreeExtrap|Source, data = DoEframeRF_airquality$WeightFrame, col = "white", layout = c(1,2))
+
+
+##############################################################
+#   Run example, NZ, random forest degree of extrapolation   #
+##############################################################
+
+# set response variable of interest
+myVar <- "FRE3"
+head(FittedDataList[[myVar]])
+
+#  calculate degree of extrapolation
+DoEframeRF_FRE3 <- RFInterpExterp(myY = names(FittedDataList[[myVar]])[1], # response variable
+                                  myX = names(FittedDataList[[myVar]])[3:10], # predictor variables
+                                  myFrame = FittedDataList[[myVar]] [FittedDataList[[myVar]]$Island == "South", ], # training data
+                                  myFrameOutside = FittedDataList[[myVar]] [FittedDataList[[myVar]]$Island == "North", ],   # new data
+                                  ntree = 500, importance = "permute") # random forest settings
+
+# add some variables for plotting
+MapVars <- c("Island","segYmidpoint","segXmidpoint")
+
+# add coordinate data for mapping
+DoEframeRF_FRE3$WeightFrame[, MapVars] <- FittedDataList[[myVar]][ match(rownames(DoEframeRF_FRE3$WeightFrame) , rownames(FittedDataList[[myVar]])) , MapVars]
+
+# histogram showing distribution of degree of extrapolation
+histogram(~DegreeExtrap|Source, data = DoEframeRF_FRE3$WeightFrame, col = "white", layout = c(1,2))
+
+# a map of degree of extrapolation.
+trellis.par.set(name = "superpose.symbol", value = list(col = rainbow(12), pch = 16))
+xyplot(segYmidpoint~ segXmidpoint|Source, data = DoEframeRF_FRE3$WeightFrame, groups = cut(DegreeExtrap, breaks=10), auto.key = list(space = "right"), scales = list(draw = F))
+
+
+
+##############################################################
+#     Run example, generic, MARS degree of extrapolation     #
+##############################################################
+
+# train a MARS model and calculate degree of extrapolaiton. 
+# Train to all data except month 5, predicted to month 5. 
+
+DoEframeMARS_airquality <- EarthInterpExterp(myY = "Ozone", # response variable
+                              myX = c("Solar.R", "Wind", "Temp"), # predictor variables 
+                              myFrame        = airquality[airquality$Month != 5, ], # training data
+                              myFrameOutside = airquality[airquality$Month == 5, ]) # new data
+
+# predictions vs degree of extrapolation
+xyplot(DegreeExtrap~PredBlind, groups = Source, data = DoEframeMARS_airquality$WeightFrame,auto.key = list(space = "right"))
+
+
+
+##############################################################
+#        Run example, NZ, MARS degree of extrapolation       #
+##############################################################
+
+myVar <- "FRE3"
+DoEframeMARS_FRE3 <- EarthInterpExterp(myY = names(FittedDataList[[myVar]])[1], # response variable
+                              myX = names(FittedDataList[[myVar]])[3:10], # predictor variables 
+                              myFrame        = FittedDataList[[myVar]] [FittedDataList[[myVar]]$Island == "South", ], # training data
+                              myFrameOutside = FittedDataList[[myVar]] [FittedDataList[[myVar]]$Island == "North", ]) # new data
+
+# predictions vs degree of extrapolation
+xyplot(DegreeExtrap~PredBlind, groups = Source, data = DoEframeMARS_FRE3$WeightFrame,auto.key = list(space = "right"))
+
+
